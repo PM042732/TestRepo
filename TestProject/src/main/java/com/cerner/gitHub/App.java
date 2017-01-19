25a230cfc9d56ef8e1a38cd6ccbbf6fbbb3dce9b
PM042732 <Preethi.MS@Cerner.com>
2017-01-19 15:10:13 +0530

    initial commit from GitDemo

--- TestProject/src/main/java/com/cerner/gitHub/App.java
+++ TestProject/src/main/java/com/cerner/gitHub/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Modified: Hello World!" );
+        System.out.println( "Modified from GitDemo workspacew: Hello World!" );
     }
 }
